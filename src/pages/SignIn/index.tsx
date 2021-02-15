a78aab8124a31356e247ab1498c19106e0a30343
Fillipe <felipesoares_1993@hotmail.com>
2021-02-15 15:32:24 -0300

    Criação do component Input e Botton da tela de login e importação do pacote de icons e estilização dos componentes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Title } from './styles';
 import logoImg from '../../assets/logo.png';
 
@@ -9,6 +12,16 @@ const SingIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+
+      <Input name="Fillipe" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button
+        onPress={() => {
+          console.log('deu bom');
+        }}
+      >
+        Entrar
+      </Button>
     </Container>
   );
 };
