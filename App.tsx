b77259b5d7b113a755bd8ea7774e53751dd9342f
Fillipe <felipesoares_1993@hotmail.com>
2021-02-15 14:24:02 -0300

    instalação de fontes com o expo e da logo da pagina de login

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { useFonts, RobotoSlab_400Regular, RobotoSlab_500Medium } from '@expo-google-fonts/roboto-slab';
+import AppLoadingPlaceholder from 'expo/build/launch/AppLoadingPlaceholder';
 import Index from './src/App';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    RobotoSlab_400Regular,
+    RobotoSlab_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoadingPlaceholder />;
+  }
   return <Index />;
 }
 
