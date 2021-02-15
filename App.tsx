83a962712ffe4449465c828112840ae60e71952a
Fillipe <felipesoares_1993@hotmail.com>
2021-02-15 11:56:03 -0300

    Instalação do EsLint, prettier e configurações de rotas iniciais coma tela de SingIn e SingOut

--- App.tsx
+++ App.tsx
@@ -1,14 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Index from './src/App';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Index />;
 }
 
 const styles = StyleSheet.create({
---
+++ src/routes/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import SingIn from '../pages/SignIn';
+import SingUp from '../pages/SignOut';
+
+const Auth = createStackNavigator();
+
+const AuthRoutes: React.FC = () => (
+  <Auth.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: '#312e38' },
+    }}
+  >
+    <Auth.Screen name="SignIn" component={SingIn} />
+    <Auth.Screen name="SignUp" component={SingUp} />
+  </Auth.Navigator>
+);
+
+export default AuthRoutes;
