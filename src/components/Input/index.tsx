7cc9668f0fdef9f1dcb1e458293cb4b3f2a86503
Fillipe <felipesoares_1993@hotmail.com>
2021-02-16 10:29:29 -0300

    Colocando foco e preenchimento nos inputs, agora quando clico em um input ele fica com uma borda e o icone muda de cor, e quando eu preencho alguma coisa no input e mudo para o proximo o icone continua com a cor alterada dizendo que aquele input esta preenchido

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
+import React, { useEffect, useRef, useImperativeHandle, forwardRef, useState, useCallback } from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
 import { Container, TextInput, Icon } from './styles';
@@ -21,6 +21,23 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
 
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isField, setIsField] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    if (inputValueRef.current.value) {
+      setIsField(true);
+    } else {
+      setIsField(false);
+    }
+  }, []);
 
   useImperativeHandle(ref, () => ({
     focus() {
@@ -45,14 +62,16 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
   }, [fieldName, registerField]);
 
   return (
-    <Container>
-      <Icon name={icon} size={20} color="#666360" />
+    <Container isFocused={isFocused}>
+      <Icon name={icon} size={20} color={isFocused || isField ? '#ff9000' : '#666360'} />
 
       <TextInput
         ref={inputElementRef}
         placeholderTextColor="#666360"
         keyboardAppearance="dark"
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={(value) => {
           // pega o texto digitado pelo usuario e insere dentro de inputValueRef
           inputValueRef.current.value = value;
